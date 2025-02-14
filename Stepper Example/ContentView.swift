804ea016cb2f18d24d61bdd297c5b5b74e5ed6df
Kevin Chen <kchen@lcs.on.ca>
2025-02-14 00:15:57 -0500

    Initial Commit

---
+++ Stepper Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stepper Example
+//
+//  Created by Haowen Chen on 2025-02-14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stepper Example/Stepper_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stepper_ExampleApp.swift
+//  Stepper Example
+//
+//  Created by Haowen Chen on 2025-02-14.
+//
+
+import SwiftUI
+
+@main
+struct Stepper_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
